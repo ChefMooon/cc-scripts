96901abc2077d590704d5aa257c2d09c65447252
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2025-01-06 14:20:27 -0500

    update legacy digOSRemote

--- digOS/main/digOSRemote.lua
+++ digOS/main/digOSRemote.lua
@@ -12,7 +12,7 @@ local broadcastFilter = "digOS"
 
 local filePath = "basalt.lua"
 if not (fs.exists(filePath)) then
-    shell.run("wget run https://basalt.madefor.cc/install.lua release latest.lua " .. filePath)
+    shell.run("wget run https://basalt.madefor.cc/install.lua release basalt-1.7.1.lua " .. filePath)
 end
 local basalt = require(filePath:gsub(".lua", ""))
 
